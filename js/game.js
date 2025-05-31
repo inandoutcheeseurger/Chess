f9f6f315fe2f8ec2d1f77ad5a27eab92fbf1d397
yaesung sho <147963792+inandoutcheeseurger@users.noreply.github.com>
2025-05-31 14:57:13 +0000

    removed log, and added a basic white pawn move (not completed yet. will continue later on)

--- js/game.js
+++ js/game.js
@@ -6,8 +6,6 @@ whosTurn.textContent = currentTurn.charAt(0).toUpperCase() + currentTurn.slice(1
 function onSquareClick(e) {
   const square = e.target;
   const piece = square.textContent;
-  console.log(square);
-  console.log(piece);
 
   if (!selectedSquareId) {
     // No piece selected yet
@@ -54,6 +52,28 @@ function onSquareClick(e) {
   
 }
 
+function findValidMoves(squareId, piece){
+  const validMoves = [];
+  const row = parseInt(squareId[1], 10); // e.g., "e2" -> 2 // 1,2,3,4 so on
+  const col = squareId.charCodeAt(0) - 97; // e.g., "e2" -> 'e' -> 4 (zero-based index) a,b,c,d so on
+
+  if (piece === '♙'){ // white pawn
+
+    validMoves.push(String.fromCharCode(col + 97) + (row + 1));
+
+    if (row === 2){ // if it is their first move, they can also go up by 2
+      validMoves.push(String.fromCharCode(col + 97) + (row + 2));
+    }
+    if (/* there is a piece in the diagnal */){
+      // add to the validMoves 
+    }
+    return filterValidMoves(validMoves);
+  }
+}
+
+function filterValidMoves(validMoves){
+  // check if the validMoves squares are not valid due to blocked by some pieces, or there is a piece of your teammates.
+}
 
 function isWhitePiece(piece) {
   return ['♙','♖','♘','♗','♕','♔'].includes(piece);
